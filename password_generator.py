b72d6d5ffbb146175b01ecbc9d73a503aafe7659
LightninKg <38631823+Koushik77@users.noreply.github.com>
2024-08-27 01:51:55 +0530

    optimization and reduction in loc

--- password_generator.py
+++ password_generator.py
@@ -9,47 +9,16 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password_length = nr_letters + nr_symbols + nr_numbers
-password = ''
-
-pwd_letters = []
-pwd_symbols = []
-pwd_numbers = []
+pwd_chars = []
 
 for i in range(0, nr_letters):
-    pwd_letters.append(letters[random.randint(0, len(letters) -1)])
+    pwd_chars.append(letters[random.randint(0, len(letters) -1)])
 for i in range(0, nr_symbols):
-    pwd_symbols.append(symbols[random.randint(0, len(symbols) -1)])
+    pwd_chars.append(symbols[random.randint(0, len(symbols) -1)])
 for i in range(0, nr_numbers):
-    pwd_numbers.append(numbers[random.randint(0, len(numbers) -1)])
+    pwd_chars.append(numbers[random.randint(0, len(numbers) -1)])
 
-#print(pwd_letters, pwd_symbols, pwd_numbers)
-for i in range(0, password_length):
-    char_type = random.randint(0, 2)
-    if char_type == 0 and len(pwd_letters) > 0:
-        letter_index = random.randint(0, len(pwd_letters) -1)
-        password += pwd_letters[letter_index]
-        pwd_letters.pop(letter_index)
-    elif char_type == 1 and len(pwd_symbols) > 0:
-        symbol_index = random.randint(0, len(pwd_symbols) -1)
-        password += pwd_symbols[symbol_index]
-        pwd_symbols.pop(symbol_index)
-    elif char_type == 2 and len(pwd_numbers) > 0:
-        number_index = random.randint(0, len(pwd_numbers) -1)
-        password += pwd_numbers[number_index]
-        pwd_numbers.pop(number_index)
-    else:
-        if len(pwd_letters) > 0:
-            letter_index = random.randint(0, len(pwd_letters) - 1)
-            password += pwd_letters[letter_index]
-            pwd_letters.pop(letter_index)
-        elif len(pwd_symbols) > 0:
-            symbol_index = random.randint(0, len(pwd_symbols) - 1)
-            password += pwd_symbols[symbol_index]
-            pwd_symbols.pop(symbol_index)
-        elif len(pwd_numbers) > 0:
-            number_index = random.randint(0, len(pwd_numbers) - 1)
-            password += pwd_numbers[number_index]
-            pwd_numbers.pop(number_index)
+random.shuffle(pwd_chars)
+password = ''.join(pwd_chars)
 
-print(password)
+print(f"Your password is {password}")
